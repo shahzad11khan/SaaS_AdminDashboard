27e99a606f4ddf0a6be97abf3e88c3e4507d5b11
as03489822 <as03489822@gmail.com>
2025-03-20 09:36:32 +0500

    chat box for compnaies

--- src/AdminPanel/Components/api/baseUri.js
+++ src/AdminPanel/Components/api/baseUri.js
@@ -1,5 +1,4 @@
-// export const baseUri = "https://saas-serversidescript.vercel.app";
-// export const baseUri = "https://saasserversidescript-production-9f7d.up.railway.app";
+
 export const baseUri = "https://saasserversidescript-production-befa.up.railway.app";
 export const notificationBaseUrl ="https://saasserversidescript-production.up.railway.app";
-// export const baseUri = "http://localhost:5000";
+//  export const baseUri = "http://localhost:5000";
