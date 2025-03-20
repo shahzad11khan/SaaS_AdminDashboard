51563865e6a18a7267b0b72e79a424078cade8fd
as03489822 <as03489822@gmail.com>
2025-03-20 10:18:05 +0500

    changes

--- src/AdminPanel/Components/api/baseUri.js
+++ src/AdminPanel/Components/api/baseUri.js
@@ -2,3 +2,4 @@
 export const baseUri = "https://saasserversidescript-production-befa.up.railway.app";
 export const notificationBaseUrl ="https://saasserversidescript-production.up.railway.app";
 //  export const baseUri = "http://localhost:5000";
+//changes
